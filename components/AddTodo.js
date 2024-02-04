50e0e2c8eb171a30827ee03ddf3c80e785b46a76
GaleArt <galeguzovaa@gmail.com>
2024-02-04 19:20:02 +0300

    stylized header

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,34 +1,67 @@
-import React from "react";
-import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
-import { db } from './firebase';
-import { collection, addDoc } from 'firebase/firestore';
+import { addDoc, collection } from 'firebase/firestore'
+import React from 'react'
+import {
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from 'react-native'
+import { db } from './firebase'
 
 export default function AddTodo() {
-    const [title, setTitle] = React.useState("");
+	const [title, setTitle] = React.useState('')
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (title !== "") {
-            await addDoc(collection(db, "todos"), {title, completed:false});
-            setTitle("");
-        };
-    };
+	const handleSubmit = async e => {
+		e.preventDefault()
+		if (title !== '') {
+			await addDoc(collection(db, 'todos'), { title, completed: false })
+			setTitle('')
+		}
+	}
 
-    return (
-        <View>
-            <TextInput
-                placeholder='Шо добавить...'
-                onChangeText={(e) => setTitle(e.target.value)}
-                style={{fontSize: 20}}
-            />
-            <TouchableOpacity onPress={handleSubmit} >
-              <Text style={{fontSize: 20}}>Добавить</Text>
-            </TouchableOpacity>
-        </View>
-    );
+	return (
+		<View style={styles.todo}>
+			<TextInput
+				placeholder='Шо добавить...'
+				onChangeText={e => setTitle(e.target.value)}
+				style={styles.input}
+			/>
+			<TouchableOpacity style={styles.button} onPress={handleSubmit}>
+				<Text style={{ fontSize: 20 }}>Добавить</Text>
+			</TouchableOpacity>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-    todo: {
-    },
+	todo: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	input: {
+		alignSelf: 'center',
+		backgroundColor: 'white',
+		color: 'black',
+		textAlign: 'center',
+		fontSize: 20,
+		margin: 5,
+		width: 250,
+		height: 40,
+		borderRadius: 10,
+	},
+	button: {
+		display: 'flex',
+		alignSelf: 'center',
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#f6f676',
+		color: 'black',
+		width: 120,
+		height: 40,
+		fontSize: 20,
+		borderRadius: 10,
+		margin: 5,
+	},
 })
